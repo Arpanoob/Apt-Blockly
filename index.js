63b1938341a8e772b61af79b84726e2668dba262
Arpan <arpanguptaastro@gmail.com>
2024-03-28 05:34:54 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/styles/index.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.css'));
+  });
+  app.get('/scripts/music_maker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, path.join('scripts','music_maker.js')));
+  });
+  app.get('/scripts/sound_blocks.js', (req, res) => {
+    res.sendFile(path.join(__dirname, path.join('scripts','sound_blocks.js')));
+  });
+  app.get('/scripts/main.js', (req, res) => {
+    res.sendFile(path.join(__dirname, path.join('scripts','main.js')));
+  });
+  app.get('/sounds/:name', (req, res) => {
+    res.sendFile(path.join(__dirname, path.join('sounds',`${req.params.name}`)));
+    //res.sendFile(path.join(__dirname, path.join('sounds','')));
+  });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ scripts/music_maker.js
@@ -0,0 +1,16 @@
+const MusicMaker = {
+  queue_: [],
+  player_: new Audio(),
+  queueSound: function (soundUrl) {
+    this.queue_.push(soundUrl);
+  },
+  play: function () {
+    const next = this.queue_.shift();
+    if (next) {
+      this.player_.src = next;
+      this.player_.play();
+    }
+  },
+};
+
+MusicMaker.player_.addEventListener('ended', MusicMaker.play.bind(MusicMaker));
